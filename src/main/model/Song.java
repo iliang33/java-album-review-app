4a9c8218a2e10d870d537ebfe77e4fbeb6002d7e
Isaac Liang <iliang03@students,cs,ubc.ca>
2025-10-02 09:22:54 -0700

    Added specification for the Song class

---
+++ src/main/model/Song.java
@@ -0,0 +1,72 @@
+package model;
+
+
+// Represents a song with a name, artist, length, rating, and a review
+public class Song {
+
+    // REQUIRES: length in the format mins:seconds, 0.0 <= rating <= 10.0
+    // EFFECTS: Creates a song with the given name, artist, length, rating, and review
+    public Song(String name, String artist, String length, double rating, String review) {
+        // stub
+
+    }
+
+
+    // REQUIRES: amount > 0.0
+    // MODIFIES: this
+    // EFFECTS: increases song rating by the given amount
+    public void increaseRating(double amount){
+        //stub
+
+    }
+
+    // REQUIRES: amount > 0.0
+    // MODIFIES: this
+    // EFFECTS: decreases song rating by the given amount
+    public void decreaseRating(double amount){
+        //stub
+
+    }
+
+    public void setName(){
+        // stub
+    }
+
+    public void setArtist(){
+        // stub
+    }
+
+    public void setLength(){
+        // stub
+    }
+
+    public void setReview(){
+        // stub
+    }
+
+    public String getName(){
+        return ""; //stub
+    }
+
+    public String getArtist(){
+        return ""; //stub
+    }
+
+    public String getlength(){
+        return ""; //stub
+    }
+
+    public double getRating(){
+        return 0.0; //stub
+    }
+
+    public String getReview(){
+        return ""; //stub
+    }
+
+    @Override
+    // EFFECTS: returns formatted information about the song as a string
+    public String toString(){
+        return "";
+    }
+}
