ee5f230db6c36dbe34726af1278b19715a0de9c7
Isaac Liang <iliang03@students,cs,ubc.ca>
2025-10-21 20:20:01 -0700

    created json test class

---
+++ src/test/persistence/JsonTest.java
@@ -0,0 +1,25 @@
+package persistence;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import ca.ubc.cs.ExcludeFromJacocoGeneratedReport;
+import model.Album;
+import model.AlbumCategory;
+
+// referenced from JsonSerializationDemo
+// https://github.students.cs.ubc.ca/CPSC210/JsonSerializationDemo.git
+
+@ExcludeFromJacocoGeneratedReport
+public class JsonTest {
+    protected void checkAlbumCategory(String name, AlbumCategory category) {
+        assertEquals(name, category.getName());
+    }
+
+    protected void checkAlbum(String name, String artist, String genre, double rating, String review, Album album) {
+        assertEquals(name, album.getName());
+        assertEquals(artist, album.getArtist());
+        assertEquals(genre, album.getGenre());
+        assertEquals(rating, album.getRating());
+        assertEquals(review, album.getReview());
+    }
+}
