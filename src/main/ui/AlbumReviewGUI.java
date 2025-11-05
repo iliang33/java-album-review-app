55c863775b142546357c60c06f051e4d9b9feaa5
Isaac Liang <iliang03@students,cs,ubc.ca>
2025-11-04 16:41:51 -0800

    added specification for Album,ReviewGUI

---
+++ src/main/ui/AlbumReviewGUI.java
@@ -0,0 +1,36 @@
+package ui;
+
+import javax.swing.*;
+
+import model.ReviewManager;
+
+// referenced from SmartHomeUI
+// https://github.students.cs.ubc.ca/CPSC210/LongFormProblemStarters.git
+
+// represents the GUI for the album review app
+public class AlbumReviewGUI extends JFrame {
+    public static final int WIDTH = 800;
+    public static final int HEIGHT = 800;
+
+    // EFFECTS: creates AlbumReviewGUI, initializes ReviewManager and shows navbar
+    // with its tabs
+    public AlbumReviewGUI() {
+
+    }
+
+    // MODIFIES: this
+    // EFFECTS: adds home, creation/removal, view tab, and update tabs to the navbar
+    public void showTabs() {
+
+    }
+
+    public ReviewManager getReviewManager() {
+        return null;
+
+    }
+
+    public JTabbedPane getNavbar() {
+        return null;
+    }
+
+}
