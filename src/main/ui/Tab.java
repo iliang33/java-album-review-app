33d126ad77ce3c834822f4140b24f12f16db02c4
Isaac Liang <iliang03@students,cs,ubc.ca>
2025-11-04 16:56:53 -0800

    added implementation for Tab

--- src/main/ui/Tab.java
+++ src/main/ui/Tab.java
@@ -4,22 +4,31 @@ import javax.swing.*;
 
 import model.ReviewManager;
 
+import java.awt.*;
+
+// referenced from SmartHomeUI
+// https://github.students.cs.ubc.ca/CPSC210/LongFormProblemStarters.git
 
 public class Tab extends JPanel {
 
-    public Tab(ReviewManager manager) {
+    private final ReviewManager manager;
 
+    public Tab(ReviewManager manager) {
+        this.manager = manager;
     }
 
     // EFFECTS: creates and returns row with button included
-    public JPanel formatButtonRow(JButton b) {
-        return null;
+    public JPanel formatButtonRow(JButton button) {
+        JPanel panel = new JPanel();
+        panel.setLayout(new FlowLayout());
+        panel.add(button);
 
+        return panel;
     }
 
     // EFFECTS: returns the ReviewManager for this tab
     public ReviewManager getManager() {
-        return null;
+        return this.manager;
     }
 
 }
