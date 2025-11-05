5f9e77fd9a43dafa1a520a29d886ffa21f0149c0
Isaac Liang <iliang03@students,cs,ubc.ca>
2025-11-04 16:54:39 -0800

    created specification for Tab

---
+++ src/main/ui/Tab.java
@@ -0,0 +1,25 @@
+package ui;
+
+import javax.swing.*;
+
+import model.ReviewManager;
+
+
+public class Tab extends JPanel {
+
+    public Tab(ReviewManager manager) {
+
+    }
+
+    // EFFECTS: creates and returns row with button included
+    public JPanel formatButtonRow(JButton b) {
+        return null;
+
+    }
+
+    // EFFECTS: returns the ReviewManager for this tab
+    public ReviewManager getManager() {
+        return null;
+    }
+
+}
