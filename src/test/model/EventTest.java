2c8bebd2ee0bdc33193350671685bab4bcd0d544
Isaac Liang <iliang03@students,cs,ubc.ca>
2025-11-22 10:47:57 -0800

    added classes and test related to event logging

---
+++ src/test/model/EventTest.java
@@ -0,0 +1,44 @@
+package model;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import ca.ubc.cs.ExcludeFromJacocoGeneratedReport;
+
+import java.util.Calendar;
+import java.util.Date;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+// referenced from AlarmSystem
+// https://github.students.cs.ubc.ca/CPSC210/AlarmSystem.git
+
+/**
+ * Unit tests for the Event class
+ */
+@ExcludeFromJacocoGeneratedReport
+public class EventTest {
+    private Event event;
+    private Date date;
+
+    // NOTE: these tests might fail if time at which line (2) below is executed
+    // is different from time that line (1) is executed. Lines (1) and (2) must
+    // run in same millisecond for this test to make sense and pass.
+
+    @BeforeEach
+    public void runBefore() {
+        event = new Event("Album review added!"); // (1)
+        date = Calendar.getInstance().getTime(); // (2)
+    }
+
+    @Test
+    public void testEvent() {
+        assertEquals("Album review added!", event.getDescription());
+        assertEquals(date, event.getDate());
+    }
+
+    @Test
+    public void testToString() {
+        assertEquals(date.toString() + "\n" + "Album review added!", event.toString());
+    }
+}
